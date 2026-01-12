f819b13487a84d5e38d8ddc471d6bb4c1020d2b5
Griffin <griffinbkeeler@gmail.com>
2026-01-12 14:52:55 -0600

    Updated docstring

--- src/augmentation/augment.py
+++ src/augmentation/augment.py
@@ -2,12 +2,25 @@ import torch
 
 
 def zero_mask(ep):
+    """
+    Randomly applies a zero-mask to samples (0-150) in each channel.
+
+    Args:
+        ep (ndarray): A sequence of time points from each channel.
+
+    Returns:
+        None
+    """
     for ch in ep:
         # The number of time points in each sample
         T = ch.shape[0]
 
         # Random sample size (0-150)
         L = int(torch.randint(0, 151, (1,)))
+
+        # Skip masking if the sample size is 0
+        if L == 0:
+            continue
 
         # Random starting position (0, T - L)
         s = int(torch.randint(0, T - L + 1, (1,)))
